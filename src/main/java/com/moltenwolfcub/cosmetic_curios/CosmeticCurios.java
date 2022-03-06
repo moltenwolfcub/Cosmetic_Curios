394ddf0afc64159e446a03aadcb310970fe9044e
MoltenWolfCub <oliver@tomlins.org.uk>
2022-03-06 09:13:02 +0000

    added curios dependency

--- src/main/java/com/moltenwolfcub/cosmetic_curios/CosmeticCurios.java
+++ src/main/java/com/moltenwolfcub/cosmetic_curios/CosmeticCurios.java
@@ -1,9 +1,15 @@
 package com.moltenwolfcub.cosmetic_curios;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,14 +19,23 @@ public class CosmeticCurios
 {
     
     public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "cosmetic_curios";
 
     public CosmeticCurios() {
         
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event){ }
+    private void setup(final FMLCommonSetupEvent event){}
+
+    private void enqueueIMC(final InterModEnqueueEvent event){
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.HEAD.getMessageBuilder().build());
+    }
+
+    private void processIMC(final InterModProcessEvent event){}
 }
